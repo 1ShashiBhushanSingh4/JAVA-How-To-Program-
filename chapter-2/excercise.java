a145d3576e2ef3174a9c747916396cbfdd1062ca
Shashi Bhushan Singh <67219349+1ShashiBhushanSingh4@users.noreply.github.com>
2022-04-26 10:06:04 +0530

    Create excercise.java

---
+++ chapter-2/excercise.java
@@ -0,0 +1,9 @@
+public class Interest {
+5 public static void main(String[] args) {
+6 double principal = 1000.0; // initial amount before interest
+7 double rate = 0.05; // interest rate
+89
+// display headers
+10 System.out.printf("%s %n", "Year", "Amount on deposit");
+11
+Fig.
